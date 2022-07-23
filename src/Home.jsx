08734c12e0dffa6277c0b3e9dec69551c0d645c3
Ayush Raj <95675158+CodeLikeAyush@users.noreply.github.com>
2022-07-23 20:28:21 +0530

    Updated About-Me Link in Home Page Button

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     return (
         <div>
             {/* <img src={hello} alt="" /> */}
-            <CommonTemplate H1="Hi there !" H2="A warm welcome by" name = "Ayush Raj" para="With a lot of efforts and learnings,I created this website using React, Material-UI and Bootstrap. Hope you would like it." imgSrc={hello} btnText="About Me" link="https://codelikeayush.github.io/porfolio-experiment/#/aboutme" targt="" />
+            <CommonTemplate H1="Hi there !" H2="A warm welcome by" name = "Ayush Raj" para="With a lot of efforts and learnings,I created this website using React, Material-UI and Bootstrap. Hope you would like it." imgSrc={hello} btnText="About Me" link="https://codelikeayush.github.io/Ayush-Portfolio/#/aboutme" targt="" />
         </div>
     )
 }
