8d2edc27966eaf6bf07f5ecd6f61a7d112fe4d82
Ayush Raj <ayushjnv25@gmail.com>
2022-09-01 23:31:11 +0530

    added Note-Taking App

--- src/projectData.jsx
+++ src/projectData.jsx
@@ -2,11 +2,11 @@ const projectData = [
     {
         id: 1,
         imgURL: 'https://lh6.googleusercontent.com/ZejasFD_uxpVLEO7bn4eh4ZnemFxzmCsJqciyw4BDcRpYdsX3Z59E4-XrfMqaSqR910=w2400',
+        seriesName: 'Chrome Extension: Toggle Dark/Light Mode',
         para1: 'Chrome Extension: Toggle between ',
         para2: 'Dark & Light Mode',
         para3: 'even if any web-page is',
         para4: 'not having dark/light mode',
-        seriesName: 'Chrome Extension: Toggle Dark/Light Mode',
         btnText: 'Github',
         Link: "https://github.com/CodeLikeAyush/Dark-Mode-Chrome-Extension",
         target: '_blank'
@@ -14,30 +14,18 @@ const projectData = [
 
     {
         id: 2,
-        imgURL: 'https://lh6.googleusercontent.com/ZejasFD_uxpVLEO7bn4eh4ZnemFxzmCsJqciyw4BDcRpYdsX3Z59E4-XrfMqaSqR910=w2400',
-        para1: 'Chrome Extension: Toggle between ',
-        para2: 'Dark & Light Mode',
-        para3: 'even if any web-page is',
-        para4: 'not having dark/light mode',
-        seriesName: 'Chrome Extension: Toggle Dark/Light Mode',
-        btnText: 'Github',
-        Link: "https://github.com/CodeLikeAyush/Dark-Mode-Chrome-Extension",
+        imgURL: 'https://lh4.googleusercontent.com/Tn61L9i3uQjTbVtd5nL-pYLzitLVCxvDPI8qGZG96xUldzaR3lJiddfjsI6jNGfnlcs=w2400',
+        seriesName: 'Note Taking Web-App : Simple & Clean UI',
+        para1: 'Simple Note Taking web app',
+        para2: 'using HTML CSS JS & Bootstrap',
+        para3: '',
+        para4: '',
+        btnText: 'Have a Glance',
+        Link: "https://codelikeayush.github.io/Note-Taking-App/",
         target: '_blank'
     },
 
-    {
-        id: 3,
-        imgURL: 'https://lh6.googleusercontent.com/ZejasFD_uxpVLEO7bn4eh4ZnemFxzmCsJqciyw4BDcRpYdsX3Z59E4-XrfMqaSqR910=w2400',
-        para1: 'Chrome Extension: Toggle between ',
-        para2: 'Dark & Light Mode',
-        para3: 'even if any web-page is',
-        para4: 'not having dark/light mode',
-        seriesName: 'Chrome Extension: Toggle Dark/Light Mode',
-        btnText: 'Github',
-        Link: "https://github.com/CodeLikeAyush/Dark-Mode-Chrome-Extension",
-        target: '_blank'
-    },
-
+    
 
 
 
