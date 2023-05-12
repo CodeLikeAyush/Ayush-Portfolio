c24d9a50665398691d5a44b3cb507d334de9d4a4
Ayush Raj <ayushjnv25@gmail.com>
2023-05-12 15:51:50 +0530

    small bug fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top fs-4 w-100">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">{<ImageAvatars />}</a>
+                    <a className="navbar-brand" href="#">{<ImageAvatars />}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
