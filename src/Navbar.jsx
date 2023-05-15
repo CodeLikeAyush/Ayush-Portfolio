ca40fc002217386a3976e66134450db6c91604a2
Ayush Raj <ayushjnv25@gmail.com>
2023-05-15 11:27:39 +0530

    ui improvement for like button and made navbar collapsed on clicking links inside it

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,11 +11,12 @@ import { NavLink } from 'react-router-dom';
 import './index.css';
 
 function Navbar() {
+
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top fs-4 w-100">
+            <nav id='navbox' className="navbar navbar-expand-lg navbar-light bg-dark fixed-top fs-4 w-100">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">{<ImageAvatars />}</a>
+                    <a className="navbar-brand" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">{<ImageAvatars />}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -23,52 +24,36 @@ function Navbar() {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <NavLink className="nav-link " aria-current="page" to="/"
-                                    activeClassName="is-active"
+                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
                                 >Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link "
                                     to="/aboutme"
-                                    activeClassName="is-active"
+                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
                                 >About Me</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link " to="/contact"
-                                    activeClassName="is-active"
+                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
                                 >Contact Me</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link " to="/certificates"
-                                    activeClassName="is-active"
+                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
                                 >Certificates</NavLink>
                             </li>
 
                             <li className="nav-item">
                                 <NavLink className="nav-link " to="/projects"
-                                    activeClassName="is-active"
+                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
                                 >Projects</NavLink>
                             </li>
 
 
-
-                            {/* <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li> */}
-
                         </ul>
 
-                        {/* <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form> */}
+
                         <ul className="navbar-nav mx-4">
                             <li className="nav-item text-light">
                                 <Button
