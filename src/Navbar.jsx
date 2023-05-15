06ef011faeb2d62509c691ba2706bbb942eb5ea9
Ayush Raj <ayushjnv25@gmail.com>
2023-05-15 11:33:07 +0530

    removed new collapsing functionality of navbar as it caused a bug

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
         <>
             <nav id='navbox' className="navbar navbar-expand-lg navbar-light bg-dark fixed-top fs-4 w-100">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">{<ImageAvatars />}</a>
+                    <a className="navbar-brand" href="#">{<ImageAvatars />}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -24,29 +24,29 @@ function Navbar() {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <NavLink className="nav-link " aria-current="page" to="/"
-                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                                    activeClassName="is-active"
                                 >Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link "
                                     to="/aboutme"
-                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                                    activeClassName="is-active"
                                 >About Me</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link " to="/contact"
-                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                                    activeClassName="is-active"
                                 >Contact Me</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link " to="/certificates"
-                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                                    activeClassName="is-active"
                                 >Certificates</NavLink>
                             </li>
 
                             <li className="nav-item">
                                 <NavLink className="nav-link " to="/projects"
-                                    activeClassName="is-active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                                    activeClassName="is-active"
                                 >Projects</NavLink>
                             </li>
 
