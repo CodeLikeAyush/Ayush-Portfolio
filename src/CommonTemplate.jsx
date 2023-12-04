f6d1bee82adf11acb15d9e0a6c6e027eb546b651
Ayush Raj <ayushjnv25@gmail.com>
2023-12-04 11:59:27 +0530

    added resume

--- src/CommonTemplate.jsx
+++ src/CommonTemplate.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 
 function CommonTemplate(props) {
-    return (
-        <>
-            <div className="showcase-area colorVar " id="home">
-                <div className="container skeleton">
-                    <div>
-                        <div className="big-title ">
-                            <h1>{props.H1}</h1>
-                            <h1>{props.H2}
-                                <br /><strong>{props.name}</strong>
-                            </h1>
-                        </div>
+  return (
+    <>
+      <div className="showcase-area colorVar " id="home">
+        <div className="container skeleton">
+          <div>
+            <div className="big-title ">
+              <h1>{props.H1}</h1>
+              <h1>
+                {props.H2}
+                <br />
+                <strong>{props.name}</strong>
+              </h1>
+            </div>
 
-                        <p className="text">
-                            {props.para}
+            <p className="text">{props.para}</p>
 
-                        </p>
+            <div className="btnDiv">
+              <a
+                href={props.link}
+                className="btn btn-lg"
+                onClick={props.scrollFunc}
+                target={props.targt}
+              >
+                {props.btnText}
+              </a>
+            </div>
+          </div>
 
-                        <div className="btnDiv" >
-                            <a href={props.link} className="btn btn-lg" onClick={props.scrollFunc} target={props.targt} >{props.btnText}</a>
-                        </div>
-                    </div>
-
-                    <div >
-                        <img src={props.imgSrc} alt="Hello" className="hello" />
-                    </div>
-                </div>
-            </div >
-        </>
-
-    );
+          <div>
+            <img src={props.imgSrc} alt="Hello" className="hello" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CommonTemplate;
-
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import CommonTemplate from './CommonTemplate';
-import hello from '../src/static/images/hello.png'
-
+import React from "react";
+import CommonTemplate from "./CommonTemplate";
+import hello from "../src/static/images/hello.png";
 
 function Home() {
-    return (
-        <div>
-            {/* <img src={hello} alt="" /> */}
-            <CommonTemplate H1="Hi there !" H2="A warm welcome by" name = "Ayush Raj" para="With a lot of efforts and learnings,I created this website using React, Material-UI and Bootstrap. Hope you would like it." imgSrc={hello} btnText="About Me" link="https://codelikeayush.github.io/Ayush-Portfolio/#/aboutme" targt="" />
-        </div>
-    )
+  return (
+    <div>
+      {/* <img src={hello} alt="" /> */}
+      {/* <CommonTemplate H1="Hi there !" H2="A warm welcome by" name = "Ayush Raj" para="With a lot of efforts and learnings,I created this website using React, Material-UI and Bootstrap. Hope you would like it." imgSrc={hello} btnText="About Me" link="https://codelikeayush.github.io/Ayush-Portfolio/#/aboutme" targt="" /> */}
+      <CommonTemplate
+        H1="Hi there !"
+        H2="A warm welcome by"
+        name="Ayush Raj"
+        para="With a lot of efforts and learnings,I created this website using React, Material-UI and Bootstrap. Hope you would like it."
+        imgSrc={hello}
+        btnText="Resume"
+        link="https://drive.google.com/file/d/16jJ18jo9DwtnIi20byrmTU_oSt9ybmRq/view?usp=sharing"
+        targt="_blank"
+      />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
